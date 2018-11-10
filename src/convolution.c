03fd1df8131234775c1c86141047856ea54d56a0
Yiming Wu <wyiming@fb.com>
2018-11-09 23:04:56 -0800

    5x5 sse2 kernel

--- src/convolution.c
+++ src/convolution.c
@@ -189,7 +189,7 @@ enum qnnp_status qnnp_create_convolution2d_nhwc_q8(
   const size_t kernel_size = kernel_height * kernel_width;
 
   uint32_t flags = 0;
-  if (kernel_size == 9 && group_input_channels == 1 && group_output_channels == 1 && groups > 1) {
+  if ((kernel_size == 9 || kernel_size == 25) && group_input_channels == 1 && group_output_channels == 1 && groups > 1) {
     flags |= QNNP_CONVOLUTION_FLAG_DW;
   } else if (kernel_size == 1 && subsampling_height == 1 && subsampling_width == 1) {
     if (group_input_channels >= qnnp_params.q8conv_xzp.kthreshold) {
