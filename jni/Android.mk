62a407da30b6e4800182da67312013edcd950da6
Marat Dukhan <marat@fb.com>
2018-11-22 12:42:22 -0800

    Add license headers in ndk-build configuration files

--- jni/Android.mk
+++ jni/Android.mk
@@ -1,3 +1,9 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
 LOCAL_PATH := $(call my-dir)/..
 
 ifeq ($(TARGET_ARCH_ABI),$(filter $(TARGET_ARCH_ABI),armeabi armeabi-v7a))
