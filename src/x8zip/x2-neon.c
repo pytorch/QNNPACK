3a9f64bb74db435d5706c9b8cb7d2a3c7f862cb0
Marat Dukhan <marat@fb.com>
2018-11-25 21:59:02 -0800

    Reformat X8ZIP micro-kernels

--- src/x8zip/x2-neon.c
+++ src/x8zip/x2-neon.c
@@ -12,36 +12,36 @@
 
 
 void qnnp_x8zip_x2__neon(
-	size_t n,
-	const void* input,
-	void* output)
+    size_t n,
+    const void* input,
+    void* output)
 {
-	const uint8_t* x = input;
-	const uint8_t* y = x + n;
-	uint8_t* o = output;
+  const uint8_t* x = input;
+  const uint8_t* y = x + n;
+  uint8_t* o = output;
 
-	if (n >= 8) {
-		do {
-			uint8x8x2_t vxy;
-			vxy.val[0] = vld1_u8(x); x += 8;
-			vxy.val[1] = vld1_u8(y); y += 8;
-			vst2_u8(o, vxy); o += 16;;
-			n -= 8;
-		} while (n >= 8);
-		if (n != 0) {
-			const size_t address_increment = n - 8;
-			uint8x8x2_t vxy;
-			vxy.val[0] = vld1_u8((const uint8_t*) ((uintptr_t) x + address_increment));
-			vxy.val[1] = vld1_u8((const uint8_t*) ((uintptr_t) y + address_increment));
-			vst2_u8((uint8_t*) ((uintptr_t) o + address_increment * 2), vxy);
-		}
-	} else {
-		do {
-			const uint8_t vx = *x++;
-			const uint8_t vy = *y++;
-			o[0] = vx;
-			o[1] = vy;
-			o += 2;
-		} while (--n != 0);
-	}
+  if (n >= 8) {
+    do {
+      uint8x8x2_t vxy;
+      vxy.val[0] = vld1_u8(x); x += 8;
+      vxy.val[1] = vld1_u8(y); y += 8;
+      vst2_u8(o, vxy); o += 16;;
+      n -= 8;
+    } while (n >= 8);
+    if (n != 0) {
+      const size_t address_increment = n - 8;
+      uint8x8x2_t vxy;
+      vxy.val[0] = vld1_u8((const uint8_t*) ((uintptr_t) x + address_increment));
+      vxy.val[1] = vld1_u8((const uint8_t*) ((uintptr_t) y + address_increment));
+      vst2_u8((uint8_t*) ((uintptr_t) o + address_increment * 2), vxy);
+    }
+  } else {
+    do {
+      const uint8_t vx = *x++;
+      const uint8_t vy = *y++;
+      o[0] = vx;
+      o[1] = vy;
+      o += 2;
+    } while (--n != 0);
+  }
 }
--- src/x8zip/x4-neon.c
+++ src/x8zip/x4-neon.c
@@ -12,46 +12,46 @@
 
 
 void qnnp_x8zip_x4__neon(
-	size_t n,
-	const void* input,
-	void* output)
+    size_t n,
+    const void* input,
+    void* output)
 {
-	const uint8_t* x = input;
-	const uint8_t* y = x + n;
-	const uint8_t* z = y + n;
-	const uint8_t* w = z + n;
-	uint8_t* o = output;
+  const uint8_t* x = input;
+  const uint8_t* y = x + n;
+  const uint8_t* z = y + n;
+  const uint8_t* w = z + n;
+  uint8_t* o = output;
 
-	if (n >= 8) {
-		do {
-			uint8x8x4_t vxyzw;
-			vxyzw.val[0] = vld1_u8(x); x += 8;
-			vxyzw.val[1] = vld1_u8(y); y += 8;
-			vxyzw.val[2] = vld1_u8(z); z += 8;
-			vxyzw.val[3] = vld1_u8(w); w += 8;
-			vst4_u8(o, vxyzw); o += 32;
-			n -= 8;
-		} while (n >= 8);
-		if (n != 0) {
-			const size_t address_increment = n - 8;
-			uint8x8x4_t vxyzw;
-			vxyzw.val[0] = vld1_u8(x + address_increment);
-			vxyzw.val[1] = vld1_u8(y + address_increment);
-			vxyzw.val[2] = vld1_u8(z + address_increment);
-			vxyzw.val[3] = vld1_u8(w + address_increment);
-			vst4_u8((uint8_t*) ((uintptr_t) o + address_increment * 4), vxyzw);
-		}
-	} else {
-		do {
-			const uint8_t vx = *x++;
-			const uint8_t vy = *y++;
-			const uint8_t vz = *z++;
-			const uint8_t vw = *w++;
-			o[0] = vx;
-			o[1] = vy;
-			o[2] = vz;
-			o[3] = vw;
-			o += 4;
-		} while (--n != 0);
-	}
+  if (n >= 8) {
+    do {
+      uint8x8x4_t vxyzw;
+      vxyzw.val[0] = vld1_u8(x); x += 8;
+      vxyzw.val[1] = vld1_u8(y); y += 8;
+      vxyzw.val[2] = vld1_u8(z); z += 8;
+      vxyzw.val[3] = vld1_u8(w); w += 8;
+      vst4_u8(o, vxyzw); o += 32;
+      n -= 8;
+    } while (n >= 8);
+    if (n != 0) {
+      const size_t address_increment = n - 8;
+      uint8x8x4_t vxyzw;
+      vxyzw.val[0] = vld1_u8(x + address_increment);
+      vxyzw.val[1] = vld1_u8(y + address_increment);
+      vxyzw.val[2] = vld1_u8(z + address_increment);
+      vxyzw.val[3] = vld1_u8(w + address_increment);
+      vst4_u8((uint8_t*) ((uintptr_t) o + address_increment * 4), vxyzw);
+    }
+  } else {
+    do {
+      const uint8_t vx = *x++;
+      const uint8_t vy = *y++;
+      const uint8_t vz = *z++;
+      const uint8_t vw = *w++;
+      o[0] = vx;
+      o[1] = vy;
+      o[2] = vz;
+      o[3] = vw;
+      o += 4;
+    } while (--n != 0);
+  }
 }
