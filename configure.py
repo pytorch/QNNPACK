0f62e4779cbb3be9f5df5a838492b098f961708e
Marat Dukhan <marat@fb.com>
2018-12-21 16:52:38 -0800

    SoftArgMax benchmark

--- configure.py
+++ configure.py
@@ -213,6 +213,7 @@ def main(args):
         build.benchmark("convolution-bench", build.cxx("convolution.cc"))
         build.benchmark("global-average-pooling-bench", build.cxx("global-average-pooling.cc"))
         build.benchmark("max-pooling-bench", build.cxx("max-pooling.cc"))
+        build.benchmark("softargmax-bench", build.cxx("softargmax.cc"))
 
         build.benchmark("q8gemm-bench", build.cxx("q8gemm.cc"))
         build.benchmark("hgemm-bench", build.cxx("hgemm.cc"))
