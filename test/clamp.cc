823b36e2f60e87141505d9d2700838191eda7309
Supriya Rao <supriyar@fb.com>
2019-08-28 09:53:56 -0700

    Fix clamp operator to work when output_min == output_max.
    Summary:
    Clamping should be ideally tightly bound to [output_min, output_min] or [output_max, output_max].
    This fails in testing when zero_point is set to 255 which is possible when all values are negative.
    
    Test Plan:
    Unit tests
    
    Reviewers:
    zaf
    Subscribers:
    
    Tasks:
    
    Tags:

--- test/clamp.cc
+++ test/clamp.cc
@@ -98,3 +98,15 @@ TEST(CLAMP_OP, small_batch_with_input_and_output_stride) {
       .testU8();
   }
 }
+
+TEST(CLAMP_OP, qmin_and_qmax_equal_uint8_max) {
+  for (size_t channels = 1; channels < 100; channels += 15) {
+    ClampOperatorTester()
+      .batchSize(3)
+      .channels(channels)
+      .qmin(255)
+      .qmax(255)
+      .iterations(3)
+      .testU8();
+  }
+}
